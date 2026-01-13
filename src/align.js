bb7d89b50b47bad7fe0556210695148f7aa2f4f1
Ony3dika <akpa3220@gmail.com>
2026-01-13 22:55:41 +0100

    first commit

---
+++ src/align.js
@@ -0,0 +1,128 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+const canvas = document.querySelector("canvas.align");
+const container = document.querySelector(".align-container");
+
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#cccccb");
+
+const textureLoader = new THREE.TextureLoader();
+const circ = textureLoader.load("/textures/circle.png");
+
+// Sizes
+const sizes = {
+  width: container.clientWidth,
+  height: container.clientHeight,
+};
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.z = 3;
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.minPolarAngle = Math.PI / 2;
+controls.maxPolarAngle = Math.PI / 2;
+
+// Mesh
+let geometry = new THREE.BufferGeometry();
+let points = new THREE.PointsMaterial({
+  size: 0.4,
+  alphaMap: circ,
+  vertexColors: true,
+  depthWrite: false,
+  transparent: true,
+  sizeAttenuation: true,
+});
+
+const count = 7000;
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+let targetColor = new THREE.Color("#0f131a");
+
+for (let i = 0; i < count * 3; i++) {
+  const i3 = i * 3;
+  positions[i] = (Math.random() - 0.5) * 10;
+
+  // Assign specific color values (R, G, B)
+  colors[i3] = targetColor.r;
+  colors[i3 + 1] = targetColor.g;
+  colors[i3 + 2] = targetColor.b;
+}
+
+geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+let mesh = new THREE.Points(geometry, points);
+scene.add(mesh);
+
+// Scroll
+window.addEventListener("wheel", (e) => {
+  const speed = 0.03;
+  camera.position.z += e.deltaY * speed;
+});
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+
+renderer.setSize(sizes.width, sizes.height, false);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Resize
+window.addEventListener("resize", () => {
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+
+  if (
+    renderer.domElement.width !== width ||
+    renderer.domElement.height !== height
+  ) {
+    renderer.setSize(width, height, false);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    sizes.width = width;
+    sizes.height = height;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+  }
+});
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Mesh Rotation
+  mesh.rotation.y = elapsedTime * 0.075;
+  mesh.rotation.z = elapsedTime * 0.025;
+
+  // Mesh Animation
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+
+    positions[i3 + 1] += 0.015;
+    if (positions[i3 + 1] > 5) positions[i3 + 1] = -5;
+  }
+  geometry.attributes.position.needsUpdate = true;
+
+  controls.update();
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
---
+++ src/hero.js
@@ -0,0 +1,133 @@
+import * as THREE from "three";
+
+const canvas = document.querySelector("canvas.webgl");
+const container = document.querySelector(".threejs-container");
+
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#0f131a");
+
+// Sizes
+const sizes = {
+  width: container.clientWidth,
+  height: container.clientHeight,
+};
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.z = 4;
+
+// Mouse
+const mouse = { x: 0.5, y: 0.5 };
+// Params
+const params = {
+  radius: 1 + mouse.x * 2,
+};
+
+// Mesh
+let geometry = new THREE.SphereGeometry(params.radius, 30, 50);
+
+let material = new THREE.PointsMaterial({
+  size: 0.02,
+  color: "#cccccb",
+  depthWrite: false,
+  blending: THREE.AdditiveBlending,
+  transparent: true,
+  sizeAttenuation: true,
+});
+
+let mesh = new THREE.Points(geometry, material);
+// mesh.rotation.z = Math.PI / 0.35;
+// mesh.rotation.x = Math.PI * 0.3;
+
+  mesh.rotation.z = Math.PI * (mouse.x * 0.5);
+  mesh.rotation.x = -(Math.PI * mouse.y);
+scene.add(mesh);
+
+// MouseMove
+window.addEventListener("mousemove", (e) => {
+  if (geometry !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(mesh);
+  }
+  mouse.x = e.clientX / sizes.width;
+  mouse.y = e.clientY / sizes.height;
+
+  if (params.radius > 1.5) {
+    params.radius -= mouse.x;
+  } else {
+    params.radius += mouse.x;
+  }
+  params.radius = mouse.x * 1.5 + 1;
+  console.log(mouse);
+
+  // Mesh
+  geometry = new THREE.SphereGeometry(params.radius, 30, 50);
+
+  material = new THREE.PointsMaterial({
+    size: 0.02,
+    color: "#cccccb",
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    sizeAttenuation: true,
+  });
+
+  mesh = new THREE.Points(geometry, material);
+  mesh.position.x = 1;
+  mesh.rotation.z = Math.PI * (mouse.x * 0.5);
+
+  scene.add(mesh);
+});
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+
+renderer.setSize(sizes.width, sizes.height, false);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Resize
+window.addEventListener("resize", () => {
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+
+  if (
+    renderer.domElement.width !== width ||
+    renderer.domElement.height !== height
+  ) {
+    renderer.setSize(width, height, false);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    sizes.width = width;
+    sizes.height = height;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+  }
+});
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Mesh animation
+  mesh.rotation.y = elapsedTime * 0.05;
+  mesh.rotation.z = Math.PI * (mouse.x * 0.5);
+  mesh.rotation.x = -(Math.PI * mouse.y);
+
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
