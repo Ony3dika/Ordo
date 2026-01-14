8b000af69f317c438e9acb52611348961b17b6ab
Ony3dika <akpa3220@gmail.com>
2026-01-14 17:37:06 +0100

    add preloader animation and icon

--- src/anim.js
+++ src/anim.js
@@ -4,41 +4,68 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
-// Hero Text Aniamtion
-let heroText = SplitText.create(".split", { type: "words" });
+// Preloader Animation
 
-gsap.from(heroText.words, {
-  yPercent: 150,
-  autoAlpha: 0,
+// Logo
+gsap.to(".ordo", {
+  yPercent: -1000,
+  duration: 1.5,
+  delay: 0.4,
+  opacity: 0.3,
+  ease: "circ.inOut",
+});
+
+// Slides
+let slides = document.querySelectorAll(".slide");
+gsap.to(slides, {
+  yPercent: -100,
+  duration: 1.5,
+  delay: 2,
+  display: "hidden",
   stagger: {
     amount: 0.5,
-  },
-  ease: "power2.out",
-  duration: 1,
-  opacity: 1,
-  delay: 1,
-  scrollTrigger: {
-    trigger: ".split",
-    start: "top 70%",
+    grid: [2, 1],
+    ease: "circ.inOut",
+    from: "center",
   },
 });
 
-// Other Text Animation
-const texts = document.querySelectorAll(".body-text");
-texts.forEach((text) => {
-  let split = SplitText.create(text, { type: "lines" });
-  gsap.from(split.lines, {
-    autoAlpha: 0,
+document.fonts.ready.then(() => {
+  // Hero Text Aniamtion
+  let heroText = SplitText.create(".split", { type: "words" });
+  gsap.from(heroText.words, {
     yPercent: 150,
-    duration: 1,
-    ease: "power2",
+    autoAlpha: 0,
     stagger: {
       amount: 0.5,
     },
+    ease: "power2.out",
+    duration: 1,
+    opacity: 1,
+    delay: 3.6,
     scrollTrigger: {
-      trigger: text,
+      trigger: ".split",
       start: "top 70%",
-      // markers: true,
     },
+  });
+
+  // Other Text Animation
+  const texts = document.querySelectorAll(".body-text");
+  texts.forEach((text) => {
+    let split = SplitText.create(text, { type: "lines" });
+    gsap.from(split.lines, {
+      autoAlpha: 0,
+      yPercent: 150,
+      duration: 1,
+      ease: "power2",
+      stagger: {
+        amount: 0.5,
+      },
+      scrollTrigger: {
+        trigger: text,
+        start: "top 70%",
+        // markers: true,
+      },
+    });
   });
 });
